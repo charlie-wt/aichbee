e0230b366aa392efd37f3dc88953efa4548eeb8c
charlie-wt <shout@charlies.computer>
2017-12-09 14:50:36 +0000

    added ability to specify list of sites to block in a separate file, optionally. added comments for some stuff

--- blocker.py
+++ blocker.py
@@ -3,7 +3,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 prnt = True
-blocklist = [
+default_blocklist = [
     'reddit.com',
     'youtube.com',
     'twitter.com',
@@ -18,25 +18,60 @@ dir = '.'
 fname = 'testhosts'
 
 class HostsHandler (FileSystemEventHandler):
+    ''' class to handle file system events, specifically for the hosts file. '''
+    # otherwise watchdog recognises itself as an edit and gets stuck responding
+    # infinitely
     fixed = False
 
-    def on_any_event (self, event ):
+    def __init__ (self, blocklist):
+        self.blocklist = blocklist
+
+    def on_any_event (self, event):
+        ''' simple print '''
         if event.src_path == dir + '/' + fname:
             if prnt: print(event.src_path+' was '+event.event_type+'!')
 
     def on_modified (self, event):
+        ''' if a file's been modified: if ti's the right file and we've not
+        already fixed it since the last change, fix it.
+        '''
         filename = event.src_path
         global fname
         global dir
         if filename == dir + '/' + fname:
             if not self.fixed:
-                refresh(filename)
+                refresh(filename, self.blocklist)
                 self.fixed = True
             else:
                 self.fixed = False
                 if prnt: print('already fixed.')
 
-def refresh (filename):
+def read_block_file (filename):
+    ''' read a list of domains to block (like 'blocklist'), from a file '''
+    blocklist = []
+
+    # get the data from the file
+    with open(filename, 'r') as f:
+        data = f.readlines()
+
+    # get just the blocked domains
+    reading_blocks = False
+    for line in data:
+        if line[-1] == '\n':
+            line = line[:-1]
+        if line == '= DOMAINS':
+            reading_blocks = True
+        elif reading_blocks:
+            if line == '===':
+                reading_blocks = False
+            else:
+                blocklist.append(line)
+
+    # return results
+    return blocklist
+
+def refresh (filename, blocklist):
+    ''' main function to correct the hosts file. '''
     # get the data from the file
     with open(filename, 'r') as f:
         data = f.readlines()
@@ -65,7 +100,11 @@ def refresh (filename):
 
 def main ():
     # specify sites
-    global blocklist
+    blocklist = []
+    try:
+        blocklist = read_block_file('blocklist.txt')
+    except:
+        blocklist = default_blocklist
     global fname
     global dir
     # add alternate forms
@@ -74,11 +113,11 @@ def main ():
     blocklist += [ item[4:] for item in orig if item.startswith('www.') ]
 
     # do an initial refresh, to get things going
-    refresh(fname)
+    refresh(fname, blocklist)
 
     # start watching file
     observer = Observer()
-    handler = HostsHandler()
+    handler = HostsHandler(blocklist)
     observer.schedule(handler, dir, recursive=True)
     observer.start()
     if prnt: print('observer started.')
@@ -87,7 +126,7 @@ def main ():
         while True:
             # check on a 1 minute timer as well, in case of undetected edits.
             time.sleep(60*1)
-            refresh(fname)
+            refresh(fname, blocklist)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
